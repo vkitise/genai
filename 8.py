9b1e8dfe85c00924f75658fd01a9e67a18a6891a
vkitise <reddyreddy04sd@gmail.com>
2026-06-20 20:52:36 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,8 +1,9 @@
+!pip install langchain-cohere
 import os
 from langchain_cohere import ChatCohere
 from langchain_core.messages import HumanMessage
 os.environ["COHERE_API_KEY"] = "N8FmJLuZKK6lkB4pkCG6SingpSBGEDWSfbI2P362"
-text = open("GenAI8.txt", encoding="utf-8").read()
+text = open("genai8.txt", encoding="utf-8").read()
 prompt = f"""
 Summary:
 Key Points:
