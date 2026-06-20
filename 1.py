3c4e25d4506f7eebb415c1230e62e777c111ef7f
vkitise <reddyreddy04sd@gmail.com>
2026-06-20 14:42:27 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,24 +7,14 @@ print(model['king'][:10])
 print("\nTop 10 words similar to king:")
 for w,s in model.most_similar('king'):
     print(f"{w}: {s:.4f}")
-r = model.most_similar(
-    positive=['king','woman'],
-    negative=['man'],
-    topn=1
-)
+r = model.most_similar(positive=['king','woman'],negative=['man'],topn=1)
 print("\nking - man + woman ≈")
 print(f"{r[0][0]} : {r[0][1]:.4f}")
 print("\nparis + italy - france ≈")
-for w,s in model.most_similar(
-    positive=['paris','italy'],
-    negative=['france']
-):
+for w,s in model.most_similar(positive=['paris','italy'],negative=['france']):
     print(f"{w}: {s:.4f}")
 print("\nwalking + swimming - walk ≈")
-for w,s in model.most_similar(
-    positive=['walking','swimming'],
-    negative=['walk']
-):
+for w,s in model.most_similar(positive=['walking','swimming'],negative=['walk']):
     print(f"{w}: {s:.4f}")
 print("\nCosine Similarity:")
 print(f"{1-cosine(model['king'],model['queen']):.4f}")
